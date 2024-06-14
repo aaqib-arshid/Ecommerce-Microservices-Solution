598e665922c0c0e1a0d7d059f89d84b232ad4190
Aaqib <aaqib.arshid@gmail.com>
2024-06-14 22:46:03 +0530

    Converted Middleware code of API Gateway.

--- API_Gateway/APIGatewayMiddleware.cs
+++ API_Gateway/APIGatewayMiddleware.cs
@@ -13,34 +13,27 @@ namespace API_Gateway
 
         public async Task Invoke(HttpContext context)
         {
-            var path = context.Request.Path.Value;
+            string? path = context.Request.Path.Value;
             var httpClient = new HttpClient();
 
-            // Route requests based on URL
-            if (path.StartsWith("/users"))
+            var response = path switch
             {
-                // Forward request to User microservice
-                var userResponse = await httpClient.GetAsync("https://demotestmicroservices-users.azurewebsites.net/");
-                await context.Response.WriteAsync(await userResponse.Content.ReadAsStringAsync());
-            }
-            else if (path.StartsWith("/products"))
+                string p when p.StartsWith("/users") => await httpClient.GetAsync("https://demotestmicroservices-users.azurewebsites.net/"),
+                string p when p.StartsWith("/products") => await httpClient.GetAsync("https://demotestmicroservices-products.azurewebsites.net/"),
+                string p when p.StartsWith("/orders") => await httpClient.GetAsync("https://demotestmicroservices-orders.azurewebsites.net/"),
+                _ => null
+            };
+
+            if (response != null)
             {
-                // Forward request to Product microservice
-                var productsResponse = await httpClient.GetAsync("https://demotestmicroservices-products.azurewebsites.net/");
-                await context.Response.WriteAsync(await productsResponse.Content.ReadAsStringAsync());
-            }
-            else if (path.StartsWith("/orders"))
-            {
-                // Forward request to Order microservice
-                var ordersResponse = await httpClient.GetAsync("https://demotestmicroservices-orders.azurewebsites.net/");
-                await context.Response.WriteAsync(await ordersResponse.Content.ReadAsStringAsync());
+                await context.Response.WriteAsync(await response.Content.ReadAsStringAsync());
             }
             else
             {
-                // Handle other routes or return 404
                 context.Response.StatusCode = (int)HttpStatusCode.NotFound;
                 await context.Response.WriteAsync("Startup API Gateway project.");
             }
+
         }
     }
 }
